c7200cc10310b532dd9668d9436ea9ba2c265092
SnavMaster09 <octav.work.dev@gmail.com>
2025-07-02 17:16:41 +0300

    chore: Updated deps + fixed eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,45 @@
-// eslint.config.js
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import vue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+import tsParser from '@typescript-eslint/parser'
 
-export default withNuxt({
-  rules: {
-    'no-unused-vars': 'error',
-    'no-console': 'warn',
-    'vue/multi-word-component-names': 'off'
+export default [
+  {
+    ignores: [
+      '.nuxt/**',
+      'node_modules/**',
+      '**/*.d.ts',
+      'dist/**',
+      'build/**'
+    ]
+  },
+  {
+    files: ['components/**/*.vue', 'pages/**/*.vue', 'layouts/**/*.vue', 'app.vue'],
+    plugins: { vue },
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    rules: {
+      ...vue.configs['flat/recommended'].rules,
+      'vue/multi-word-component-names': 'off',
+      'no-console': 'warn',
+      'no-unused-vars': 'warn'
+    }
+  },
+  {
+    files: ['composables/**/*.ts', 'utils/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': 'warn'
+    }
   }
-})
+]
