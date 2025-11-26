547b9d73fce8222cf33ac4abbe29bc10a894f383
SnavMaster09 <octav.work.dev@gmail.com>
2025-11-26 17:40:28 +0200

    send placeholder functional msg

--- app/pages/arhitect/chat/[id].vue
+++ app/pages/arhitect/chat/[id].vue
@@ -1,9 +1,31 @@
 <template>
-    <div>
-        {{ $route.params.id }}
-    </div>
+    <button @click="sendMessage"> 
+        Click me
+    </button>
 </template>
 
 <script setup>
-    const roomId = $route.params.id
+    import { io } from 'socket.io-client'
+    
+    const route = useRoute()
+    const roomId = route.params.id
+
+    const { auth } = useUser()
+    const senderId = auth.value.id
+    const socket = io('http://localhost:3002')
+    
+    onMounted(() => {
+        
+        socket.emit("join-room", roomId)
+
+    })
+
+    function sendMessage() {
+        socket.emit("send-message", {
+        roomId,
+        text: "Hello!",
+        senderId
+});
+    }
+
 </script>
